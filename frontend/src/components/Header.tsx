4614aee59f93043002eabdedc102c4481d1f8521
bobeu <bobmatea27@gmail.com>
2025-11-19 17:44:03 +0100

    Commented out ConnectionStatus for now

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -84,7 +84,7 @@ export default function Header() {
                   largeScreen: 'full',
                 }}
               />
-              <ConnectionStatus />
+              {/* <ConnectionStatus /> */}
               
               <Tooltip.Tooltip>
                 <Tooltip.TooltipTrigger asChild>
